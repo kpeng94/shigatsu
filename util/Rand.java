1b76e8b74b9c72fdaa8f043a83274898011d7cab
Rene Zhang <rz99@cornell.edu>
2015-01-06 00:16:01 -0500

    Added random

---
+++ util/Rand.java
@@ -0,0 +1,35 @@
+package bytecodeTester;
+
+/**
+ * 
+ * @author Rene
+ * 
+ * Class for handling random integer generation
+ * Initialize with a seed using the constructor
+ * 
+ * nextInt() returns a random integer
+ * nextInt(int max) returns a nonnegative integer less than max
+ * nextAnd(int n) returns a nonnegative integer bitwise anded with n
+ *
+ */
+public class Rand {
+	static final long a = 0xffffda61L;
+	private long x;
+	
+	public Rand(long seed) {
+		x = seed & 0xffffffffL;
+	}
+	
+	public int nextInt() {
+		return (int) ((a * (x & 0xffffffffL)) + (x >>> 32));
+	}
+	
+	public int nextInt(int max) {
+		return (int) (((a * (x & 0xffffffffL)) + (x >>> 32)) & 0x7fffffff) % max;
+	}
+	
+	public int nextAnd(int n) {
+		return (int) (((a * (x & 0xffffffffL)) + (x >>> 32)) & n);
+	}
+	
+}

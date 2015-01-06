f204257dd37c0d42a873ef8cce8146d9bb1da83a
Rene Zhang <rz99@cornell.edu>
2015-01-06 00:21:34 -0500

    added generic base bot

---
+++ util/RobotPlayer.java
@@ -0,0 +1,17 @@
+package dummyBot;
+
+import battlecode.common.*;
+
+/**
+ * 
+ * @author Rene
+ * 
+ * Generic Base Bot
+ *
+ */
+public class RobotPlayer {
+	public static void run(RobotController rc) {
+		while(true) {
+		}
+	}
+}

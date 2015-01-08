498c02cb4a6842ad57b207093fa0a8d99f26985d
Kevin Peng <kpeng94@mit.edu>
2015-01-07 19:06:25 -0500

    Copying my trash bot to tankBotv1

---
+++ teams/tankBotv1/MinerRobot.java
@@ -0,0 +1,9 @@
+package tankBotv1;
+
+import battlecode.common.*;
+
+public class MinerRobot {
+	public static void run(RobotController rc) throws Exception {
+		
+	}
+}
---
+++ teams/tankBotv1/RobotPlayer.java
@@ -0,0 +1,38 @@
+package tankBotv1;
+
+import battlecode.common.*;
+
+public class RobotPlayer {
+
+	public static void run(RobotController rc) throws Exception {
+		switch(rc.getType()) {
+			case HQ:
+				HQRobot.run(rc);
+				break;
+			case BEAVER:
+				BeaverRobot.run(rc);
+				break;
+			case MINER:
+				MinerRobot.run(rc);
+				break;
+			case SOLDIER:
+				SoldierRobot.run(rc);
+				break;
+			case TANK:
+				TankRobot.run(rc);
+				break;
+			case TOWER:
+				TowerRobot.run(rc);
+				break;
+			case TANKFACTORY:
+				TankFactoryRobot.run(rc);
+				break;
+			case BARRACKS:
+				BarracksRobot.run(rc);
+				break;
+				
+			default:
+				throw new Exception("Bad robot.");				
+		}
+	}
+}

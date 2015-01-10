08c3e93893350757ab35089d9ac0b04ac717bc95
Genghis Chau <gchau@mit.edu>
2015-01-10 04:20:25 -0500

    Added a concentric square searcher to miningBotTest's MinerRobot.java

---
+++ teams/miningBotTest/MinerRobot.java
@@ -0,0 +1,109 @@
+package miningBotTest;
+
+import java.util.Random;
+
+import battlecode.common.*;
+
+public class MinerRobot extends Robot {
+	public static Direction directionToEnemyHQ;
+	public static RobotInfo[] enemies;
+	static int numberOfLiveBeavers = 1; // one for itself
+	static MapLocation rallyPoint;
+	static boolean rallied = false;
+	static int job = 0;
+	static int directionToEnemyHQInt;
+	static int distanceToEnemyHQ;
+	private static int numBarracksBuilt = 0;
+	static int[] offsets = { 0, 1, -1, 2, -2, 3, -3, 4 };
+
+	public static void init(RobotController rc) throws GameActionException {
+		rand = new Random(rc.getID());
+		myRange = rc.getType().attackRadiusSquared;
+		myTeam = rc.getTeam();
+		enemyTeam = myTeam.opponent();
+		myTeamHQ = rc.senseHQLocation();
+		enemyTeamHQ = rc.senseEnemyHQLocation();
+		directionToEnemyHQ = myTeamHQ.directionTo(enemyTeamHQ);
+		directionToEnemyHQInt = directionToInt(directionToEnemyHQ);
+		distanceToEnemyHQ = myTeamHQ.distanceSquaredTo(enemyTeamHQ);
+		rallyPoint = MapUtils.pointSection(myTeamHQ, enemyTeamHQ, 0.75);
+	}
+
+	public static void run(RobotController controller) throws Exception {
+		rc = controller;
+		init(rc);
+		while (true) {
+			myLocation = rc.getLocation();
+			directionFromHQ = myTeamHQ.directionTo(myLocation);
+//			rc.yield();
+//			if (rc.isWeaponReady() && decideAttack()) {
+//				attack();
+//			} else if (rc.isCoreReady()) {
+//				rc.setIndicatorString(0, "core is ready");
+//				rc.setIndicatorString(1, "ores: " + rc.senseOre(myLocation)
+//						+ " "
+//						+ (rc.senseOre(myLocation) >= ORE_THRESHOLD_MINER));
+//				rc.setIndicatorString(2, "thresh: " + ORE_THRESHOLD_MINER + " "
+//						+ rc.canMine());
+//				if (rc.senseOre(myLocation) >= ORE_THRESHOLD_MINER
+//						&& rc.canMine()) {
+//					rc.mine();
+//				} else {
+//					double max = 0;
+//					Direction maxDirection = directionFromHQ;
+//					for (Direction d : directions) {
+//						double ore = rc.senseOre(rc.getLocation().add(d));
+//						if (ore > max) {
+//							max = ore;
+//							maxDirection = d;
+//						}
+//					}
+//					tryMove(maxDirection);
+//				}
+//			}
+			if(Clock.getRoundNum() == 1000){
+				findClosestMinableOre(rc, 99, 6);
+			}
+			rc.yield();
+		}
+	}
+
+	/**
+	 * Calculates the closest square with at least the threshold amount of 
+	 * ore. The distance is calculated in terms of Manhattan distance and NOT
+	 * Euclidean distance. This does NOT factor in the square the robot is currently on.
+	 * 
+	 * @param rc - RobotController for the robot
+	 * @param threshold - the minimum amount of ore for the function to return
+	 * @param stepLimit - the size of the search outwards (a step limit of n will search in
+	 * 					  a [n by n] square, centered about the robot's current location
+	 * @return - MapLocation of closest square with ore greater than the threshold, 
+	 *           or null if there is none
+	 */
+	public static MapLocation findClosestMinableOre(RobotController rc,
+			double threshold, int stepLimit) {
+		int step = 1;
+		int currentDirection = 0;
+		MapLocation currentLocation = rc.getLocation();
+
+		while (step < stepLimit) {
+			for (int i = 0; i < step; i++) {
+				currentLocation = currentLocation
+						.add(directions[currentDirection]);
+				if (rc.senseOre(currentLocation) > threshold)
+					return currentLocation;
+			}
+			currentDirection = (currentDirection + 2) % 8;
+			for (int i = 0; i < step; i++) {
+				currentLocation = currentLocation.add(directions[currentDirection]);
+				if (rc.senseOre(currentLocation) > threshold)
+					return currentLocation;
+			}
+			currentDirection = (currentDirection + 2) % 8;
+			
+			step++;
+		}
+
+		return null;
+	}
+}
---
+++ teams/miningBotTest/TankFactoryRobot.java
@@ -0,0 +1,57 @@
+package miningBotTest;
+
+import java.util.Random;
+
+import battlecode.common.*;
+
+public class TankFactoryRobot extends Robot {
+	public static Direction directionToEnemyHQ;
+	public static int distanceToEnemyHQ;
+	
+	public static void init(RobotController rc) throws GameActionException {
+        rand = new Random(rc.getID());
+		myRange = rc.getType().attackRadiusSquared;
+		myTeam = rc.getTeam();
+		enemyTeam = myTeam.opponent();
+		myTeamHQ = rc.senseHQLocation();
+		enemyTeamHQ = rc.senseEnemyHQLocation();
+		directionToEnemyHQ = myTeamHQ.directionTo(enemyTeamHQ);
+		distanceToEnemyHQ = myTeamHQ.distanceSquaredTo(enemyTeamHQ);
+	}
+	
+	public static void run(RobotController controller) throws Exception {
+		rc = controller;
+		init(rc);
+		while (true) {
+			if (rc.isCoreReady() && rc.getTeamOre() >= RobotType.TANK.oreCost) {
+				trySpawn(directionToEnemyHQ, RobotType.TANK);				
+			}
+
+			rc.yield();		
+		}
+		
+	}
+	
+	static int directionToInt(Direction d) {
+		switch (d) {
+			case NORTH:
+				return 0;
+			case NORTH_EAST:
+				return 1;
+			case EAST:
+				return 2;
+			case SOUTH_EAST:
+				return 3;
+			case SOUTH:
+				return 4;
+			case SOUTH_WEST:
+				return 5;
+			case WEST:
+				return 6;
+			case NORTH_WEST:
+				return 7;
+			default:
+				return -1;
+		}
+	}	
+}

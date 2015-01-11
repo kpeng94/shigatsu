2d2adb78d9bc9ecc814b3156c3ae159972098d95
Bo Yuan Zhou <bz88@cornell.edu>
2015-01-11 04:03:05 -0500

    modified and tested scout method against all maps

--- teams/droneScoutOmega/UDroneHandler.java
+++ teams/droneScoutOmega/UDroneHandler.java
@@ -1,5 +1,7 @@
 package droneScoutOmega;
 
+import java.util.Arrays;
+
 import battlecode.common.*;
 
 public class UDroneHandler extends UnitHandler {
@@ -7,24 +9,25 @@ public class UDroneHandler extends UnitHandler {
 	private static Direction heading;
 	
 	private static double mainAxisSlope;
-	private static double mainAxisIntercept;
 	private static double symmAxisSlope;
-	private static double symmAxisIntercept;
+	
+	private static MapLocation refIntercept;
 	
 	private enum ScoutDir {
 		POS, NEG;
 	}
 	private enum ScoutPhase {
 		TO_MID,
-		SWEEP_AXIS_POS, SWEEP_AXIS_NEG,
-		SWEEP_AXIS_POS_BACK, SWEEP_AXIS_NEG_BACK,
+		SWEEP_AXIS_TO,
+		SWEEP_AXIS_BACK,
 		TO_HQ,
 		SWEEP_LRTB;
 	}
 	
 	private static ScoutPhase curPhase;
 	private static int counter;
-	private static Direction lrtbDir;
+	private static Direction[] validLRTBDirs;
+	private static Direction curLRTBDir;
 	private static ScoutDir scoutDir;
 	
 	public static void loop(RobotController rcon) {
@@ -49,26 +52,21 @@ public class UDroneHandler extends UnitHandler {
 	protected static void init(RobotController rcon) throws GameActionException {
 		initUnit(rcon);
 		curPhase = ScoutPhase.TO_MID;
+		refIntercept = new MapLocation((myHQ.x + enemyHQ.x) / 2, (myHQ.y + enemyHQ.y) / 2);
 		
 		// Calculating the slope of the main axis that both HQs lie on
 		if (myHQ.x == enemyHQ.x) {
 			mainAxisSlope = Double.POSITIVE_INFINITY;
 		} else {
-			mainAxisSlope = (myHQ.y - enemyHQ.y) / (myHQ.x - enemyHQ.x);
+			mainAxisSlope = 1.0 * (myHQ.y - enemyHQ.y) / (myHQ.x - enemyHQ.x);
 		}
-		
-		// Calculating the intercept of the symmetry axis using my hq
-		mainAxisIntercept = myHQ.y - mainAxisSlope * myHQ.x;
 		
 		// Calculating the slope of the symmetry axis
 		if (myHQ.y == enemyHQ.y) {
 			symmAxisSlope = Double.POSITIVE_INFINITY;
 		} else {
-			symmAxisSlope = (myHQ.x - enemyHQ.x) / (enemyHQ.y - myHQ.y);
+			symmAxisSlope = 1.0 * (myHQ.x - enemyHQ.x) / (enemyHQ.y - myHQ.y);
 		}
-		
-		// Calculating the intercept of the symmetry axis using the midpoint of the hqs
-		symmAxisIntercept = ((myHQ.y + enemyHQ.y) / 2) - symmAxisSlope * ((myHQ.x + enemyHQ.x) / 2);
 		
 		if (Comm.readBlock(Comm.getDroneId(), 0) == 1) {
 			scoutDir = ScoutDir.POS;
@@ -77,37 +75,31 @@ public class UDroneHandler extends UnitHandler {
 		}
 		
 		counter = 0;
-		lrtbDir = Direction.NONE;
+		curLRTBDir = Direction.NONE;
+		
+		validLRTBDirs = new Direction[4];
+		for (int i = 0; i < 4; i++) {
+			validLRTBDirs[i] = Direction.NONE;
+		}
 	}
 
 	protected static void execute() throws GameActionException {
 		executeUnit();
-		rc.setIndicatorString(2, curPhase.toString());
 		MapUtils.updateBounds(myLoc);
-		if (rc.isWeaponReady()) {
-			inRangeEnemies = rc.senseNearbyRobots(typ.attackRadiusSquared,
-					otherTeam);
-			tryAttack();
-		}
 		if (rc.isCoreReady()) {
+			rc.setIndicatorString(1, "" + Arrays.toString(validLRTBDirs));
 			switch (curPhase) {
 			case TO_MID:
 				toMid();
 				break;
-			case SWEEP_AXIS_POS:
-				sweepAxisPos();
-				break;
-			case SWEEP_AXIS_NEG:
-				sweepAxisNeg();
+			case SWEEP_AXIS_TO:
+				sweepAxisTo();
 				break;
 			case SWEEP_LRTB:
 				sweepLRTB();
 				break;
-			case SWEEP_AXIS_POS_BACK:
-				sweepAxisPosBack();
-				break;
-			case SWEEP_AXIS_NEG_BACK:
-				sweepAxisNegBack();
+			case SWEEP_AXIS_BACK:
+				sweepAxisBack();
 				break;
 			case TO_HQ:
 				toHQ();
@@ -116,138 +108,83 @@ public class UDroneHandler extends UnitHandler {
 			NavSimple.walkTowards(heading);
 		}
 	}
-
-	protected static void tryAttack() throws GameActionException {
-		if (inRangeEnemies.length > 0) {
-			MapLocation minLoc = inRangeEnemies[0].location;
-			int minRange = myLoc.distanceSquaredTo(minLoc);
-			for (int i = inRangeEnemies.length - 1; i > 0; i--) { // Get minimum
-																	// in array
-				RobotInfo enemy = inRangeEnemies[i];
-				MapLocation enemyLoc = enemy.location;
-				int enemyRange = myLoc.distanceSquaredTo(enemyLoc);
-				if (enemyRange < minRange) {
-					minRange = enemyRange;
-					minLoc = enemyLoc;
-				}
-			}
-
-			rc.attackLocation(minLoc);
-		}
-	}
 	
 	protected static void toMid() throws GameActionException {
-		MapLocation midPoint = new MapLocation((myHQ.x + enemyHQ.x) / 2, (myHQ.y + enemyHQ.y) / 2);
-		if (myLoc.distanceSquaredTo(midPoint) < 2) {
-			if (scoutDir == ScoutDir.POS) {
-				curPhase = ScoutPhase.SWEEP_AXIS_POS;
-				sweepAxisPos();
-			} else {
-				curPhase = ScoutPhase.SWEEP_AXIS_NEG;
-				sweepAxisNeg();
-			}
+		if (myLoc.distanceSquaredTo(refIntercept) < 2) {
+			curPhase = ScoutPhase.SWEEP_AXIS_TO;
+			sweepAxisTo();
 		} else {
-			heading = myLoc.directionTo(midPoint);
+			heading = myLoc.directionTo(refIntercept);
 		}
 	}
 	
-	protected static void sweepAxisPos() throws GameActionException {
-		int nextX = myLoc.x + 1;
+	protected static void sweepAxisTo() throws GameActionException {
+		int nextX = myLoc.x;
+		if (scoutDir == ScoutDir.POS) {
+			nextX++;
+		} else {
+			nextX--;
+		}
+		
 		Direction[] dirs = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
 		for (Direction dir: dirs) {
 			MapLocation test = myLoc.add(dir, 4);
 			if (rc.senseTerrainTile(test) == TerrainTile.OFF_MAP) {
-				// Find first LRTB direction, discard if results in enemy territory, to off map, or back towards midpoint
-				if (lrtbDir == Direction.NONE) {
+				// Find valid LRTB directions, discard if results in enemy territory, to off map, or back towards midpoint
+				if (curLRTBDir == Direction.NONE) {
+					int numValidDir = 0;
 					for (Direction dir2: dirs) {
 						MapLocation test2 = myLoc.add(dir2, 4);
 						if (rc.senseTerrainTile(test2) == TerrainTile.OFF_MAP) {
 							continue;
 						} else if (test2.distanceSquaredTo(enemyHQ) < test2.distanceSquaredTo(myHQ)) {
 							continue;
-						} else if (dir2 == test2.directionTo(new MapLocation((myHQ.x + enemyHQ.x) / 2, (myHQ.y + enemyHQ.y) / 2))) {
+						} else if (dir2 == test2.directionTo(refIntercept)) {
 							continue;
 						} else {
-							lrtbDir = dir2;
-							break;
+							validLRTBDirs[numValidDir] = dir2;
+							numValidDir++;
 						}
 					}
 				}
+				curLRTBDir = tangentDir(dir, validLRTBDirs);
 				curPhase = ScoutPhase.SWEEP_LRTB;
 				sweepLRTB();
 				break;
 			}
 		}
-		double tempIntercept = myLoc.y - symmAxisSlope * myLoc.x;
-		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + tempIntercept)));
-	}
-	
-	protected static void sweepAxisNeg() throws GameActionException {
-		int nextX = myLoc.x - 1;
-		Direction[] dirs = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
-		for (Direction dir: dirs) {
-			MapLocation test = myLoc.add(dir, 4);
-			if (rc.senseTerrainTile(test) == TerrainTile.OFF_MAP) {
-				// Find first LRTB direction, discard if results in enemy territory, to off map, or back towards midpoint
-				if (lrtbDir == Direction.NONE) {
-					for (Direction dir2: dirs) {
-						MapLocation test2 = myLoc.add(dir2, 4);
-						if (rc.senseTerrainTile(test2) == TerrainTile.OFF_MAP) {
-							continue;
-						} else if (test2.distanceSquaredTo(enemyHQ) < test2.distanceSquaredTo(myHQ)) {
-							continue;
-						} else if (dir2 == test2.directionTo(new MapLocation((myHQ.x + enemyHQ.x) / 2, (myHQ.y + enemyHQ.y) / 2))) {
-							continue;
-						} else {
-							lrtbDir = dir2;
-							break;
-						}
-					}
-				}
-				curPhase = ScoutPhase.SWEEP_LRTB;
-				sweepLRTB();
-				break;
-			}
-		}
-		double tempIntercept = myLoc.y - symmAxisSlope * myLoc.x;
-		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + tempIntercept)));
+		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + (refIntercept.y - symmAxisSlope * refIntercept.x))));
 	}
 	
 	protected static void sweepLRTB() throws GameActionException {
 		
 		if (counter == 9) {
 			counter = 0;
-			if (scoutDir == ScoutDir.POS) {
-				curPhase = ScoutPhase.SWEEP_AXIS_POS_BACK;
-				sweepAxisPosBack();
-			} else {
-				curPhase = ScoutPhase.SWEEP_AXIS_NEG_BACK;
-				sweepAxisNegBack();
-			}
+			curPhase = ScoutPhase.SWEEP_AXIS_BACK;
+			sweepAxisBack();
 		} else {
-			heading = lrtbDir;
+			// change the direction if we reached an edge
+			MapLocation test = myLoc.add(curLRTBDir, 4);
+			if (rc.senseTerrainTile(test) == TerrainTile.OFF_MAP) {
+				curLRTBDir = tangentDir(curLRTBDir,validLRTBDirs);
+			}
+			heading = curLRTBDir;
+			refIntercept = refIntercept.add(heading);
 			counter++;
 		}
 	}
 
-	protected static void sweepAxisPosBack() throws GameActionException {
-		int nextX = myLoc.x - 1;
-		double tempIntercept = myLoc.y - symmAxisSlope * myLoc.x;
-		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + tempIntercept)));
-		MapLocation test = myLoc.add(heading);
-		int projection = (int) (test.x * mainAxisSlope + mainAxisIntercept);
-		if (Math.abs(test.y - projection) <= 1) {
-			curPhase = ScoutPhase.TO_HQ;
-			toHQ();
+	protected static void sweepAxisBack() throws GameActionException {
+		int nextX = myLoc.x;
+		if (scoutDir == ScoutDir.POS) {
+			nextX--;
+		} else {
+			nextX++;
 		}
-	}
-	
-	protected static void sweepAxisNegBack() throws GameActionException {
-		int nextX = myLoc.x + 1;
-		double tempIntercept = myLoc.y - symmAxisSlope * myLoc.x;
-		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + tempIntercept)));
+		
+		heading = myLoc.directionTo(new MapLocation(nextX, (int) (nextX * symmAxisSlope + (refIntercept.y - symmAxisSlope * refIntercept.x))));
 		MapLocation test = myLoc.add(heading);
-		int projection = (int) (test.x * mainAxisSlope + mainAxisIntercept);
+		int projection = (int) (test.x * mainAxisSlope + (myHQ.y - mainAxisSlope * myHQ.x));
 		if (Math.abs(test.y - projection) <= 1) {
 			curPhase = ScoutPhase.TO_HQ;
 			toHQ();
@@ -258,16 +195,22 @@ public class UDroneHandler extends UnitHandler {
 		
 		if (counter == 7) {
 			counter = 0;
-			if (scoutDir == ScoutDir.POS) {
-				curPhase = ScoutPhase.SWEEP_AXIS_POS;
-				sweepAxisPos();
-			} else {
-				curPhase = ScoutPhase.SWEEP_AXIS_NEG;
-				sweepAxisNeg();
-			}
+			curPhase = ScoutPhase.SWEEP_AXIS_TO;
+			sweepAxisTo();
 		} else {
 			heading = myLoc.directionTo(myHQ);
+			refIntercept = refIntercept.add(heading);
 			counter++;
 		}
+	}
+	
+	protected static Direction tangentDir(Direction ray, Direction[] validDirs) {
+		for (Direction dir: validDirs) {
+			if ((ray == Direction.NORTH || ray == Direction.SOUTH) && (dir == Direction.WEST || dir == Direction.EAST))
+				return dir;
+			if ((ray == Direction.WEST || ray == Direction.EAST) && (dir == Direction.NORTH || dir == Direction.SOUTH))
+				return dir;
+		}
+		return validDirs[0];
 	}
 }

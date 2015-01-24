07a00e07e1669d91bcf6996eb5804a0359a6229a
Rene Zhang <rz99@cornell.edu>
2015-01-24 16:20:43 -0500

    commander bot, nav done

---
+++ teams/commanderBot/Utils.java
@@ -0,0 +1,13 @@
+package commanderBot;
+
+public class Utils {
+    
+    public static <T> String printArray(T[] array) {
+        StringBuilder printout = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            printout.append(i + ": " + array[i] + " ");
+        }
+        return printout.toString();
+    }
+
+}

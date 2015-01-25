0e94b754106fb80c5545f9be40bae2d2ff3745eb
Bo Yuan Zhou <bz88@cornell.edu>
2015-01-25 12:44:42 -0500

    starting missile wave micro

---
+++ teams/missileWave/Utils.java
@@ -0,0 +1,13 @@
+package missileWave;
+
+public class Utils {
+    
+    public static <T> String printArray(T[] array) {
+        StringBuilder printout = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            printout.append(i + ": " + array[i] + " ");
+        }
+        return printout.toString();
+    }
+
+}

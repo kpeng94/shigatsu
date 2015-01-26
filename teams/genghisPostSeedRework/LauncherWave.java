5924331a0324752d946602d0c6e84364c322ee14
Genghis Chau <gchau@mit.edu>
2015-01-26 02:59:47 -0500

    Added JavaDocs to LauncherWave

--- teams/genghisPostSeedRework/LauncherWave.java
+++ teams/genghisPostSeedRework/LauncherWave.java
@@ -4,13 +4,15 @@ import battlecode.common.*;
 
 public class LauncherWave {
 
+    // Currently 6 squadrons; can be changed to any number.
     private static final int[] WAVE_OFFSETS = { 100, 101, 102, 103, 104, 105 };
     public static int squadronNumber = -1;
 
-    private static final int ODD_COUNT_MASK  = 0x003F0000;
-    private static final int EVEN_COUNT_MASK = 0x0FC00000;
-    private static final int COMBINE_MASK    = 0x70000000;
-    private static final int LOC_MASK        = 0x0000FFFF;
+    // Use these masks for interpreting squadron data.
+    public static final int ODD_COUNT_MASK  = 0x003F0000;
+    public static final int EVEN_COUNT_MASK = 0x0FC00000;
+    public static final int COMBINE_MASK    = 0x70000000;
+    public static final int LOC_MASK        = 0x0000FFFF;
 
     public static void setWave(int waveNum, int data) throws GameActionException {
         Comm.writeBlock(Comm.getLauncherId(), WAVE_OFFSETS[waveNum], data);
@@ -20,6 +22,10 @@ public class LauncherWave {
         return Comm.readBlock(Comm.getLauncherId(), WAVE_OFFSETS[waveNum]);
     }
 
+    /**
+     * Launchers update the count number for their squadron number (a role call).
+     * @throws GameActionException
+     */
     public static void launcherRoleCall() throws GameActionException {
         if(squadronNumber == -1)
             return;
@@ -33,6 +39,12 @@ public class LauncherWave {
         }
     }
 
+    /**
+     * Launcher updates the count number for their squadron number (a role call). Call this one
+     * if you've already gotten the squadron data sometime previously (to save a Comm read call).
+     * @param data
+     * @throws GameActionException
+     */
     public static void launcherRoleCall(int data) throws GameActionException {
         if(squadronNumber == -1)
             return;
@@ -45,10 +57,20 @@ public class LauncherWave {
         }
     }
 
+    /**
+     * Wrapper for setting your squadron number. Should only be called by launchers.
+     * @param num
+     */
     public static void setSquadNumber(int num) {
         squadronNumber = num;
     }
 
+    /**
+     * Returns the number of launchers in a specific squadron. This data is behind by 1 turn.
+     * @param squadNum
+     * @return
+     * @throws GameActionException
+     */
     public static int getSquadCount(int squadNum) throws GameActionException {
         int data = Comm.readBlock(Comm.getLauncherId(), WAVE_OFFSETS[squadNum]);
         if (Clock.getRoundNum() % 2 == 0) {
@@ -57,6 +79,11 @@ public class LauncherWave {
         return (data & EVEN_COUNT_MASK) >>> 22;
     }
 
+    /**
+     * Resets the appropriate launcher count for each squadron. Should be called by the HQ at the beginning of 
+     * each round.
+     * @throws GameActionException
+     */
     public static void resetCounts() throws GameActionException {
         int rnd = Clock.getRoundNum();
         for (int i = 6; --i >= 0;) {
